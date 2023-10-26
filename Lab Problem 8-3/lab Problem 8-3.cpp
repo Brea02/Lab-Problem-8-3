268db13f7a5da67e9070b427bc3502673e8649f5
Breanna <b-sproul@onu.edu>
2023-10-25 20:21:42 -0700

    completed convertRomanToInt function

--- Lab Problem 8-3/lab Problem 8-3.cpp
+++ Lab Problem 8-3/lab Problem 8-3.cpp
@@ -4,41 +4,79 @@
 
 using namespace std;
 
-int romanCharValue(char r);
+int romanCharValue(string r);
+int convertRomanToInt(string s);
 
 int main(){
-	cout << "Enter Roman Numerals: ";
-	char numeral;
-	cin >> numeral;
+	//cout << "Enter Roman Numerals: ";
+	//char numeral;
+	//cin >> numeral;
+	string s;
+	while (true) {
+		cout << "Enter Roman Numerals or Q to quit: ";
+		getline(cin, s);
+		if (s == "Q") break;
+		cout << s << " = " << convertRomanToInt(s) << endl;
+	}
 
-	cout << numeral << " = " << romanCharValue(numeral) << endl;
+	//cout << numeral << " = " << romanCharValue(numeral) << endl;
 }
 
 
-int romanCharValue(char r) {
-	int sum = 0;
-	if (r == 'I') {
-		sum += 1;
+
+int romanCharValue(string r) {
+	int num = 0;
+	if (r == "I") {
+		num += 1;
 	}
-	else if (r == 'V') {
-		sum += 5;
+	else if (r == "V") {
+		num += 5;
 	}
-	else if (r == 'X') {
-		sum += 10;
+	else if (r == "X") {
+		num += 10;
 	}
-	else if (r == 'L') {
-		sum += 50;
+	else if (r == "L") {
+		num += 50;
 	}
-	else if (r == 'C') {
-		sum += 100;
+	else if (r == "C") {
+		num += 100;
 	}
-	else if (r == 'D') {
-		sum += 500;
+	else if (r == "D") {
+		num += 500;
 	}
-	else if (r == 'M') {
-		sum += 1000;
+	else if (r == "M") {
+		num += 1000;
 	}
 
-	return sum;
+	return num;
 }
 
+
+int convertRomanToInt(string s) {
+	int total = 0;
+	int x = 0;
+	int num_char = s.length();
+	string last_character = s.substr(s.length() - 1, 1);
+	int num_last = s.length() - 1;
+
+	for (x = 0; x <= num_last; x++) {
+		string cut = s.substr(x, 1);
+		string cut2 = s.substr((x+1), 1);
+		int second = romanCharValue(cut2);
+		int first = romanCharValue(cut);
+
+		if (num_char == 1) {
+			total += first;
+			return total;
+		}
+		else if (first >= second) {
+			total += first;
+		}
+		else if (first < second) {
+			total += (second - first);
+			x++;
+		}
+	}
+
+	return total;
+}
